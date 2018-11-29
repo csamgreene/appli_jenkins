dbae0557baeb7b9ae3238889e23c267f91477c85
Christopher Greene <chris.greene@luxresearchinc.com>
2018-11-29 16:44:47 -0500

    Add batch to at1_bs

--- spec/at1_browserstack_spec.rb
+++ spec/at1_browserstack_spec.rb
@@ -24,7 +24,11 @@ describe 'Applitools and BrowserStack' , :type=>:feature, :js=>true do
         
         # Applitools api key.
         eyes.api_key = 'd1hQAwqXH04jtJ5wGIej01whl1108x4xRPK3SvsmXP3fA110'
-            
+
+        b = Applitools::BatchInfo.new "Simple AT - Jenkins Tests"
+        b.id = "1234"
+        eyes.batch = b  
+                    
         # Start visual testing with browser viewport set to 1024x768.
         eyes.test(app_name: 'Applitools-Browserstack-Jenkins1', test_name: 'Applitools-Browserstack-Jenkins1',
                   viewport_size: {width: 800, height: 600}, driver: driver) do
