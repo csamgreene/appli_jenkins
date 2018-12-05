9e1ed165acbe6c51725e3a38b6671e8f9b1cbe93
Christopher Greene <chris.greene@luxresearchinc.com>
2018-12-05 13:58:03 -0500

    Make current

--- app/jenkins_shell_scripts/rspec.sh
+++ app/jenkins_shell_scripts/rspec.sh
@@ -4,4 +4,6 @@ rvm use 2.3
 
 gem install bundler
 bundle install
-bundle exec rspec
+
+bundle exec rake ci:bump_build # Adds git tag for each successful build
+bundle exec rspec --format RspecJunitFormatter --out rspec.xml. #Formats output in a format Jenkins can understand
